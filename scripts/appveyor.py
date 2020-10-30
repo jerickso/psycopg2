308ff7a60bcb26687e6278a2c2961eba020851c6
Jason Erickson <jerickso@stickpeople.com>
2020-10-30 12:04:14 -0600

    Combine options onto one line

--- scripts/appveyor.py
+++ scripts/appveyor.py
@@ -333,10 +333,8 @@ def build_psycopg():
     run_python(
         ["setup.py", "build_ext", "--have-ssl"]
         + ["-l", "libpgcommon", "-l", "libpgport"]
-        + ["-L", opt.ssl_build_dir / 'lib']
-        + ["-L", opt.pg_build_dir / 'lib']
-        + ['-I', opt.ssl_build_dir / 'include']
-        + ['-I', opt.pg_build_dir / 'include']
+        + ["-L", opt.ssl_build_dir / 'lib;' + opt.pg_build_dir / 'lib']
+        + ['-I', opt.ssl_build_dir / 'include;' +  opt.pg_build_dir / 'include']
     )
     run_python(["setup.py", "build_py"])
 
